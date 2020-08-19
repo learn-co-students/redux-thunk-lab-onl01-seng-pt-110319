f3cc36ba230a321228523539c58c6c0eac9bec5f
kierra <51520037+kierrakay@users.noreply.github.com>
2020-08-19 12:57:23 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -2,8 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom'
 import App from './App'
 
+import { createStore, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import thunk from 'redux-thunk';
+import catsReducer from './reducers/catsReducer.js'
+
+
+const store = createStore(catsReducer, applyMiddleware(thunk))
 
 ReactDOM.render(
-    <App />,
+    <Provider store={store}>
+    <App />
+    </Provider>,
     document.getElementById('root')
 );
+
+// We pass this reducer into createStore, 
+// assigning the return value to store. Then, 
+// we'll wrap the App component in Provider and
+//  pass store to it:
