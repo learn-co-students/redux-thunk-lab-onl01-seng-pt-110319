95f1354a7ab8cfc951303664675f97eda6a8ca81
Jordan <jordanleelong5@gmail.com>
2020-08-29 16:44:54 -0700

    Done.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchCats } from './actions/catActions'
 
 class App extends Component {   
+  componentDidMount() {
+    console.log(this.props) // this will fire second with our catsPics object
+    this.props.fetchCats()
+  }
   
   render() {
+    console.log(this.props.catPics) // this will fire first with an empty array, then after CDM it comes back here with our cat pics info
     return (
       <div>
         <h1>CatBook</h1>
@@ -12,5 +19,18 @@ class App extends Component {
   }
 }
 
-export default App
+const mapStateToprops = state => {
+  return {
+    catPics: state.cats, 
+    loading: state.loading
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    fetchCats: () => dispatch(fetchCats())
+  }
+}
+
+export default connect(mapStateToprops, mapDispatchToProps)(App)
 
