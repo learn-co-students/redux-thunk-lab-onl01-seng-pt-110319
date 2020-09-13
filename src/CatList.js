bcd0e08f0d28c98fa5defad610128436cb4f8afe
Wills Blake <richwblake@gmail.com>
2020-09-12 23:12:01 -0400

    Done.

--- src/CatList.js
+++ src/CatList.js
@@ -1,1 +1,14 @@
 // write your CatList component here
+import React, { Component } from 'react'
+
+export default class CatList extends Component {
+    renderCats = () => this.props.catPics.map(cat => <img key={cat.id} alt={cat.id} src={cat.url}></img>)
+
+    render() {
+        return(
+            <div>
+                {this.renderCats()}
+            </div>
+        )
+    }
+};
