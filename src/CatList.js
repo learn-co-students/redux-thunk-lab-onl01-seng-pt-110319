5070373bea0022a26d4fc20e4e2f800cd3e61754
Aida Abdikulova <aabdikul@Aidas-MBP.fios-router.home>
2020-09-13 15:21:19 -0400

    Done.

--- src/CatList.js
+++ src/CatList.js
@@ -1,1 +1,23 @@
 // write your CatList component here
+import React, { Component } from 'react'
+
+class CatList extends Component {
+
+	render() {
+
+		const pic = this.props.catPics.map((cat) => {
+			return <li key={cat.id}> <img src={cat.url} alt={cat.url}/> </li>
+		})
+
+		return (
+			<div>
+			{pic}
+			</div>
+		)
+	}
+}
+
+export default CatList
+
+
+  
