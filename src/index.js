1d5510d8c06bbe5ad903e855933ba1b2c84081d5
Alouiscious Media <info@reneesoul.com>
2020-09-25 15:41:13 -0700

    added store with provider and thunk

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
 import App from './App'
+import { createstore, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import catsReducer from '../reducers/CatsReducer'
+import thunk from "redux-thunk";
 
+
+let store = createstore(catsReducer, applyMiddleware(thunk))
 
 ReactDOM.render(
-    <App />,
+    <Provider store={store}>
+        <App />
+    </Provider>,
     document.getElementById('root')
 );
