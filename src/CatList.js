f60e47c9947d7705e51ce2875cef2868da396701
github username <github email address>
2020-09-29 15:24:17 -0700

    Done.

--- src/CatList.js
+++ src/CatList.js
@@ -1,31 +1,31 @@
-// import React from 'react';
-
-// const CatList = ({ cats }) => {
-    
-//     const catImages = cats.map(cat => <img key={cat.id} src={cat.url} atl={cat.id} />)
-//     return (
-//         <div>
-//             {catImages}
-//         </div>
-//     )
-// }
-
-// export default CatList;
-
 import React from 'react';
 
-class CatList extends React.Component {
-  listCats = () => {
-    return this.props.catPics.map(cat => <img key={cat.id} src={cat.url} alt={cat.id} />)
-  }
-
-  render() {
+const CatList = ({ cats }) => {
+    
+    const catImages = cats.map(cat => <img key={cat.id} src={cat.url} atl={cat.id} />)
     return (
-      <div>
-        {this.listCats()}
-      </div>
+        <div>
+            {catImages}
+        </div>
     )
-  }
 }
 
 export default CatList;
+
+// import React from 'react';
+
+// class CatList extends React.Component {
+//   listCats = () => {
+//     return this.props.catPics.map(cat => <img key={cat.id} src={cat.url} alt={cat.id} />)
+//   }
+
+//   render() {
+//     return (
+//       <div>
+//         {this.listCats()}
+//       </div>
+//     )
+//   }
+// }
+
+// export default CatList;
